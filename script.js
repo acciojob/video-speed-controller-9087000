930d2bfe3adf3fb09f36a59ec84603d24528c69e
9087000 <50361103+9087000@users.noreply.github.com>
2024-12-07 05:36:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,60 +1,19 @@
-// Get elements
 const video = document.querySelector('.viewer');
-const toggle = document.querySelector('.toggle');
-const progress = document.querySelector('.progress');
-const progressFilled = document.querySelector('.progress__filled');
-const volumeSlider = document.querySelector('input[name="volume"]');
-const playbackSpeedSlider = document.querySelector('input[name="playbackSpeed"]');
-const rewindButton = document.querySelector('.rewind');
-const forwardButton = document.querySelector('.forward');
+const speed = document.querySelector('.speed');
+const speedBar = document.querySelector('.speed-bar');
 
-// Functions
-function togglePlay() {
-    const method = video.paused ? 'play' : 'pause';
-    video[method]();
+const minSpeed = 0.5;
+const maxSpeed = 4;
+
+function handleSpeedChange(e) {
+  const y = e.pageY - speed.offsetTop;
+  const percent = y / speed.offsetHeight;
+  const playbackRate = percent * (maxSpeed - minSpeed) + minSpeed;
+  video.playbackRate = playbackRate;
+  speedBar.style.height = `${percent * 100}%`;
+  speedBar.textContent = `${playbackRate.toFixed(2)}×`;
 }
 
-function updateButton() {
-    const icon = video.paused ? '►' : '❚ ❚';
-    toggle.textContent = icon;
-}
-
-function handleVolumeChange() {
-    video.volume = volumeSlider.value;
-}
-
-function handlePlaybackSpeedChange() {
-    video.playbackRate = playbackSpeedSlider.value;
-}
-
-function handleProgress() {
-    const percent = (video.currentTime / video.duration) * 100;
-    progressFilled.style.width = `${percent}%`;
-}
-
-function scrub(e) {
-    const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
-    video.currentTime = scrubTime;
-}
-
-function rewind() {
-    video.currentTime -= 10;
-}
-
-function forward() {
-    video.currentTime += 25;
-}
-
-// Event listeners
-video.addEventListener('click', togglePlay);
-video.addEventListener('play', updateButton);
-video.addEventListener('pause', updateButton);
-video.addEventListener('timeupdate', handleProgress);
-
-toggle.addEventListener('click', togglePlay);
-volumeSlider.addEventListener('input', handleVolumeChange);
-playbackSpeedSlider.addEventListener('input', handlePlaybackSpeedChange);
-
-progress.addEventListener('click', scrub);
-rewindButton.addEventListener('click', rewind);
-forwardButton.addEventListener('click', forward);
+// Event listener for mouse movements on the speed bar
+speed.addEventListener('mousemove', handleSpeedChange);
+speed.addEventListener('click', handleSpeedChange);
